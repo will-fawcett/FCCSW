0abfcc3e5c1c638f02c08739b314a174a05eda0a
Valentin Volkl <valentin.volkl@cern.ch>
2017-04-02 23:53:41 +0200

    Average over phi in materialscan

--- Detector/DetComponents/src/MaterialScan.cpp
+++ Detector/DetComponents/src/MaterialScan.cpp
@@ -3,6 +3,8 @@
 
 #include "GaudiKernel/ITHistSvc.h"
 #include "GaudiKernel/Service.h"
+#include "GaudiKernel/RndmGenerators.h"
+#include "GaudiKernel/IRndmGenSvc.h"
 
 #include "DD4hep/LCDD.h"
 #include "DD4hep/Printout.h"
@@ -17,6 +19,7 @@ MaterialScan::MaterialScan(const std::string& name, ISvcLocator* svcLoc) : Servi
   declareProperty("filename", m_filename, "file name to save the tree to");
   declareProperty("etaBinning", m_etaBinning, "eta bin size");
   declareProperty("etaMax", m_etaMax, "maximum eta value");
+  declareProperty("nPhiTrials", m_nPhiTrials, "number of random phi values to average over");
 }
 
 StatusCode MaterialScan::initialize() {
@@ -29,10 +32,19 @@ StatusCode MaterialScan::initialize() {
     return StatusCode::FAILURE;
   }
 
+  SmartIF<IRndmGenSvc> randSvc;
+  randSvc = service("RndmGenSvc");
+  StatusCode sc = m_flatPhiDist.initialize(randSvc, Rndm::Flat(0., M_PI / 2.));
+  if (sc.isFailure()) {
+    return sc;
+  }
+  
+
   std::unique_ptr<TFile> rootFile(TFile::Open(m_filename.c_str(), "RECREATE"));
   // no smart pointers possible because TTree is owned by rootFile (root mem management FTW!)
   TTree* tree = new TTree("materials", "");
   double eta = 0;
+  double phi = 0;
   unsigned nMaterials = 0;
   std::unique_ptr<std::vector<double>> nX0(new std::vector<double>);
   std::unique_ptr<std::vector<double>> nLambda(new std::vector<double>);
@@ -57,31 +69,37 @@ StatusCode MaterialScan::initialize() {
   std::array<Double_t, 3> pos = {0, 0, 0};
   std::array<Double_t, 3> dir = {0, 0, 0};
   TVector3 vec(0, 0, 0);
+  for (int iPhi = 0; iPhi < m_nPhiTrials; ++iPhi) {
+      //phi = m_flatPhiDist();
+      phi = 0;
   for (eta = -m_etaMax; eta < m_etaMax; eta += m_etaBinning) {
-    nX0->clear();
-    nLambda->clear();
-    matDepth->clear();
-    material->clear();
+      nX0->clear();
+      nLambda->clear();
+      matDepth->clear();
+      material->clear();
 
-    vec.SetPtEtaPhi(1, eta, 0);
-    auto n = vec.Unit();
-    dir = {n.X(), n.Y(), n.Z()};
-    double distance = worldVol->DistFromInside(pos.data(), dir.data());
+      
+      vec.SetPtEtaPhi(1, eta, phi);
+      auto n = vec.Unit();
+      dir = {n.X(), n.Y(), n.Z()};
+      double distance = worldVol->DistFromInside(pos.data(), dir.data());
 
-    DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
-    debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
-            << ") <=> eta = " << eta << endmsg;
-    const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
-    nMaterials = materials.size();
+      DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
+      debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
+              << ") <=> eta = " << eta << ", phi =  " << phi  <<  endmsg;
+      const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
+      nMaterials = materials.size();
 
-    for (unsigned i = 0, n = materials.size(); i < n; ++i) {
-      TGeoMaterial* mat = materials[i].first->GetMaterial();
-      material->push_back(mat->GetName());
-      matDepth->push_back(materials[i].second);
-      nX0->push_back(materials[i].second / mat->GetRadLen());
-      nLambda->push_back(materials[i].second / mat->GetIntLen());
-    }
+      for (unsigned i = 0, n = materials.size(); i < n; ++i) {
+        TGeoMaterial* mat = materials[i].first->GetMaterial();
+        material->push_back(mat->GetName());
+        matDepth->push_back(materials[i].second ); // / static_cast<double>(m_nPhiTrials));
+        nX0->push_back(materials[i].second / mat->GetRadLen()); // / static_cast<double>(m_nPhiTrials));
+        nLambda->push_back(materials[i].second / mat->GetIntLen()); // / static_cast<double>(m_nPhiTrials));
+      }
     tree->Fill();
+      }
+
   }
   tree->Write();
   rootFile->Close();
