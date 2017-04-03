036b7435f13c98e4baf74f55d958443157f3f6ec
Valentin Volkl <valentin.volkl@cern.ch>
2017-04-04 01:27:11 +0200

    Use map to average material over phi

--- Detector/DetComponents/src/MaterialScan.cpp
+++ Detector/DetComponents/src/MaterialScan.cpp
@@ -69,36 +69,38 @@ StatusCode MaterialScan::initialize() {
   std::array<Double_t, 3> pos = {0, 0, 0};
   std::array<Double_t, 3> dir = {0, 0, 0};
   TVector3 vec(0, 0, 0);
-  for (int iPhi = 0; iPhi < m_nPhiTrials; ++iPhi) {
-      //phi = m_flatPhiDist();
-      phi = 0;
   for (eta = -m_etaMax; eta < m_etaMax; eta += m_etaBinning) {
       nX0->clear();
       nLambda->clear();
       matDepth->clear();
       material->clear();
 
-      
-      vec.SetPtEtaPhi(1, eta, phi);
-      auto n = vec.Unit();
-      dir = {n.X(), n.Y(), n.Z()};
-      double distance = worldVol->DistFromInside(pos.data(), dir.data());
+      std::map<DD4hep::Geometry::Material, double> phiAveragedMaterialsBetween;
+      for (int iPhi = 0; iPhi < m_nPhiTrials; ++iPhi) {
+          phi = m_flatPhiDist();
+          //phi = 0;
+        vec.SetPtEtaPhi(1, eta, phi);
+        auto n = vec.Unit();
+        dir = {n.X(), n.Y(), n.Z()};
+        double distance = worldVol->DistFromInside(pos.data(), dir.data());
 
-      DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
-      debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
-              << ") <=> eta = " << eta << ", phi =  " << phi  <<  endmsg;
-      const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
-      nMaterials = materials.size();
-
-      for (unsigned i = 0, n = materials.size(); i < n; ++i) {
-        TGeoMaterial* mat = materials[i].first->GetMaterial();
+        DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
+        debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
+                << ") <=> eta = " << eta << ", phi =  " << phi  <<  endmsg;
+        const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
+        for (unsigned i = 0, n = materials.size(); i < n; ++i) {
+          phiAveragedMaterialsBetween[materials[i].first] += materials[i].second / static_cast<double>(m_nPhiTrials);
+      }
+      }
+      nMaterials = materialPhiAverage.size();
+      for (auto matpair: phiAveragedMaterialsBetween) {
+        TGeoMaterial* mat = matpair.first->GetMaterial();
         material->push_back(mat->GetName());
-        matDepth->push_back(materials[i].second ); // / static_cast<double>(m_nPhiTrials));
-        nX0->push_back(materials[i].second / mat->GetRadLen()); // / static_cast<double>(m_nPhiTrials));
-        nLambda->push_back(materials[i].second / mat->GetIntLen()); // / static_cast<double>(m_nPhiTrials));
+        matDepth->push_back(matpair.second ); 
+        nX0->push_back(matpair.second / mat->GetRadLen()); 
+        nLambda->push_back(matpair.second / mat->GetIntLen()); 
       }
     tree->Fill();
-      }
 
   }
   tree->Write();
