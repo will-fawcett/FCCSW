71afee5c91bc31e4265b6014f06abe0f569d1dae
Valentin Volkl <valentin.volkl@cern.ch>
2017-04-09 19:03:10 +0200

    Reformat base config files

--- TrkValidation/python/geantFullsimBaseConfig.py
+++ TrkValidation/python/geantFullsimBaseConfig.py
@@ -5,14 +5,23 @@ from Gaudi.Configuration import *
 from Configurables import FCCDataSvc
 podioevent = FCCDataSvc("EventDataSvc")
 
+# DD4hep geometry service
+# Parses the given xml file
+from Configurables import GeoSvc
+geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
+  'file:Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml'],
+                    OutputLevel = DEBUG)
 
 
+### Generation ################################################################
 from Configurables import ConstPileUp
-pileuptool = ConstPileUp(numPileUpEvents=0)#, Filename="Generation/data/Pythia_minbias_pp_100TeV.cmd")
+pileuptool = ConstPileUp(numPileUpEvents=0)
 
 from Configurables import PythiaInterface
 pythiafile="Generation/data/Pythia_minbias_pp_100TeV.cmd"
-pythia8gen = PythiaInterface("Pythia8Interface", Filename=pythiafile, OutputLevel=INFO)
+pythia8gen = PythiaInterface("Pythia8Interface", 
+    Filename=pythiafile, 
+    OutputLevel=INFO)
 pythia8gen.PileUpTool = pileuptool
 pythia8gen.hepmc.Path = "hepmc"
 
@@ -22,12 +31,7 @@ hepmc_converter.hepmc.Path="hepmc"
 hepmc_converter.genparticles.Path="allGenParticles"
 hepmc_converter.genvertices.Path="allGenVertices"
 
-# DD4hep geometry service
-# Parses the given xml file
-from Configurables import GeoSvc
-geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
-  'file:Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml'],
-                    OutputLevel = DEBUG)
+### Simulation ################################################################
 
 from Configurables import SimG4Svc
 geantservice = SimG4Svc(
@@ -40,7 +44,7 @@ geantservice = SimG4Svc(
 from Configurables import SimG4ConstantMagneticFieldTool
 field = SimG4ConstantMagneticFieldTool("SimG4ConstantMagneticFieldTool", FieldOn=True, IntegratorStepper="ClassicalRK4")
 
-from Configurables import SimG4Alg, SimG4SaveTrackerHits, SimG4PrimariesFromEdmTool
+from Configurables import SimG4SaveTrackerHits
 savetrackertool = SimG4SaveTrackerHits(
   "saveTrackerHits", 
   readoutNames = [
@@ -50,14 +54,20 @@ savetrackertool = SimG4SaveTrackerHits(
   )
 savetrackertool.positionedTrackHits.Path = "positionedHits"
 savetrackertool.trackHits.Path = "hits"
+
+from Configurables import SimG4PrimariesFromEdmTool
 particle_converter = SimG4PrimariesFromEdmTool("EdmConverter")
 particle_converter.genParticles.Path = "allGenParticles"
+
 from Configurables import SimG4SingleParticleGeneratorTool
 pgun = SimG4SingleParticleGeneratorTool("GeantinoGun", etaMin=-5, etaMax=5, particleName="geantino")
+
+from Configurables import SimG4Alg
 geantsim = SimG4Alg("SimG4Alg",
                     outputs = ["SimG4SaveTrackerHits/saveTrackerHits"],
                     eventProvider=particle_converter)
 
+### Persistency ###############################################################
 from Configurables import PodioOutput
 out = PodioOutput("out", OutputLevel=DEBUG)
 out.outputCommands = ["keep *"]
@@ -67,10 +77,11 @@ out.outputCommands = ["keep *"]
 topAlgList = [pythia8gen, hepmc_converter, geantsim, out]
 svcList = [podioevent, geoservice, geantservice]
 
+# actual jobs need to add the application mgr like this
 #from Configurables import ApplicationMgr
-#ApplicationMgr( TopAlg = TopAlg,
+#ApplicationMgr( TopAlg = TopAlgList,
 #                EvtSel = 'NONE',
 #                EvtMax   = 1,
-#                ExtSvc = [podioevent, geoservice, geantservice],
+#                ExtSvc = svcList
 #                OutputLevel=DEBUG
 # )
--- TrkValidation/python/geantinoBaseConfig.py
+++ TrkValidation/python/geantinoBaseConfig.py
@@ -6,13 +6,13 @@ podioevent = FCCDataSvc("EventDataSvc")
 
 # DD4hep geometry service
 # Parses the given xml file
-from Configurables import GeoSvc, SimG4SingleParticleGeneratorTool
+from Configurables import GeoSvc
 geoservice = GeoSvc("GeoSvc", 
                     detectors=[
                       'file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
                       'file:TrkValidation/compact/Tracker.xml'
                       ],
-                    OutputLevel = DEBUG
+                    OutputLevel = INFO
                     )
 
 
@@ -29,21 +29,21 @@ geantservice = SimG4Svc("SimG4Svc",
 # lots, lots, lots of debug information
 #geantservice.G4commands += ["/tracking/verbose 1"]
 
-# Geant4 algorithm
-# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
-from Configurables import SimG4Alg, SimG4SaveTrackerHits
 
 from Configurables import SimG4ConstantMagneticFieldTool
 field = SimG4ConstantMagneticFieldTool("SimG4ConstantMagneticFieldTool", FieldOn=True, IntegratorStepper="ClassicalRK4")
+
 # first, create a tool that saves the tracker hits
 # Name of that tool in GAUDI is "XX/YY" where XX is the tool class name ("SimG4SaveTrackerHits")
 # and YY is the given name ("saveTrackerHits")
+from Configurables import SimG4SaveTrackerHits
 savetrackertool = SimG4SaveTrackerHits("saveTrackerHits",
                                        readoutNames=[ "TrackerBarrelReadout", "TrackerEndcapReadout"]
                                        )
 savetrackertool.positionedTrackHits.Path = "positionedHits"
 savetrackertool.trackHits.Path = "hits"
-# next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
+
+from Configurables import SimG4SingleParticleGeneratorTool
 pgun = SimG4SingleParticleGeneratorTool("GeantinoGun", 
                                         etaMin=-6, 
                                         etaMax=6, 
@@ -51,6 +51,11 @@ pgun = SimG4SingleParticleGeneratorTool("GeantinoGun",
                                         saveEdm=True,
                                         )
 
+
+# Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
+from Configurables import SimG4Alg
+# create the G4 algorithm, giving the list of names of tools ("XX/YY")
 geantsim = SimG4Alg("SimG4Alg",
                     outputs= ["SimG4SaveTrackerHits/saveTrackerHits" ],
                     eventProvider=pgun
