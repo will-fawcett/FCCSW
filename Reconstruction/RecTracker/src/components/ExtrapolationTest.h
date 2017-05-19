bca6ce8da010f9d804c659c0a6891b2d28aa4088
Valentin Volkl <valentin.volkl@cern.ch>
2017-05-19 15:46:24 +0200

    Add track extrapolation algorithm and test

---
+++ Reconstruction/RecTracker/src/components/ExtrapolationTest.h
@@ -0,0 +1,56 @@
+#ifndef RECTRACKER_EXTRAPOLATIONTEST_H
+#define RECTRACKER_EXTRAPOLATIONTEST_H
+
+// GAUDI
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/ToolHandle.h"
+
+// FCCSW
+#include "FWCore/DataHandle.h"
+class IGeoSvc;
+class ITrackingGeoSvc;
+class ITrkGeoDumpSvc;
+class ITrackSeedingTool;
+
+namespace Acts {
+class TrackingGeometry;
+class IExtrapolationEngine;
+}
+
+namespace fcc {
+class TrackHitCollection;
+class PositionedTrackHitCollection;
+}
+
+class ExtrapolationTest : public GaudiAlgorithm {
+public:
+  ExtrapolationTest(const std::string& name, ISvcLocator* svcLoc);
+
+  ~ExtrapolationTest();
+
+  StatusCode initialize();
+
+  StatusCode execute();
+
+  StatusCode finalize();
+
+private:
+  /// Pointer to the geometry service
+  SmartIF<ITrackingGeoSvc> m_trkGeoSvc;
+  SmartIF<IGeoSvc> m_geoSvc;
+
+  std::shared_ptr<Acts::TrackingGeometry> m_trkGeo;
+  std::shared_ptr<const Acts::IExtrapolationEngine> m_exEngine;
+
+  DataHandle<fcc::PositionedTrackHitCollection> m_positionedTrackHits{"positionedHits", Gaudi::DataHandle::Writer,
+                                                                      this};
+  std::default_random_engine m_e;
+};
+
+using namespace Acts;
+
+typedef FittableMeasurement<long int> FitMeas_t;
+template <ParID_t... pars>
+using Meas_t = Measurement<long int, pars...>;
+
+#endif /* RECTRACKER_EXTRAPOLATIONTEST_H */
