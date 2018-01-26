729d00ada2e4b142a7cfc9024d43894fadf6b794
Valentin Volkl <valentin.volkl@cern.ch>
2018-01-26 15:07:00 +0100

    Rename test

---
+++ Reconstruction/RecTracker/src/components/TrackFitTest.cpp
@@ -0,0 +1,81 @@
+
+#include "DetInterface/IGeoSvc.h"
+
+#include "datamodel/PositionedTrackHitCollection.h"
+#include "datamodel/TrackHitCollection.h"
+
+#include "DD4hep/Detector.h"
+#include "DD4hep/Volumes.h"
+#include "DDRec/API/IDDecoder.h"
+#include "DDSegmentation/BitField64.h"
+#include "DDSegmentation/CartesianGridXZ.h"
+
+#include <cmath>
+#include <random>
+
+#include "TrackFitTest.h"
+#include "RecInterface/ITrackSeedingTool.h"
+#include "RecTracker/TrackingUtils.h"
+#include "tricktrack/RiemannFit.h"
+
+using namespace tricktrack;
+
+DECLARE_ALGORITHM_FACTORY(TrackFitTest)
+
+TrackFitTest::TrackFitTest(const std::string& name, ISvcLocator* svcLoc)
+    : GaudiAlgorithm(name, svcLoc) {
+
+  declareProperty("positionedTrackHits", m_positionedTrackHits, "Tracker hits (Input)");
+}
+
+StatusCode TrackFitTest::initialize() {
+  info() << "initialize" << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TrackFitTest::execute() {
+
+  // get hits from event store
+  const fcc::PositionedTrackHitCollection* fcchits = m_positionedTrackHits.get();
+  // filter primary particle and create hit matrix
+  //
+  //
+  constexpr unsigned int nhits = 4;
+
+
+  // do the actual fit
+  //
+  Matrix3xNd riemannHits = Matrix3xNd::Zero(3,nhits);
+  /*for (unsigned int i = 0; i < nhits; ++i) {
+    riemannHits.col(i) << i, i, i;
+  }
+  */
+  riemannHits.col(0) << 1, 0, 0;
+  riemannHits.col(1) << 0, 1, 1;
+  riemannHits.col(2) << -1, 0, 2;
+  riemannHits.col(3) <<  0, -1, 3;
+
+
+
+  
+  std::cout << riemannHits << std::endl;
+  std::cout << "Fast fit: " << std::endl;
+  std::cout << Fast_fit(riemannHits) << std::endl; 
+          
+  Matrix3Nd hits_cov = Matrix3Nd::Random(3*nhits,3*nhits);
+  auto h = Helix_fit(riemannHits, hits_cov, 1);
+  std::cout << "charge " << h.q << std::endl;
+  std::cout << "chi_2 circle " << h.chi2_circle << std::endl;
+  std::cout << "chi_2 line " << h.chi2_line << std::endl;
+  std::cout << "par " << h.par << std::endl; 
+  //
+
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TrackFitTest::finalize() {
+  StatusCode sc = GaudiAlgorithm::finalize();
+  return sc;
+}
--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 # set FCCSWBASEDIR to the directory containing this script
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /cvmfs/fcc.cern.ch/testing/sw/views/stable/x86_64-slc6-gcc62-opt/setup.sh
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/afs/cern.ch/work/v/vavolkl/public/fcc.cern.ch/sw/0.9.0/tricktrack/install
+export CMAKE_PREFIX_PATH=/afs/cern.ch/work/v/vavolkl/public/fcc.cern.ch/sw/0.9.0/tricktrack/install:$CMAKE_PREFIX_PATH
 
