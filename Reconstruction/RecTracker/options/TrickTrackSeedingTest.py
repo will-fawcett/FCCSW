e5fc4f0665315067e10d7d0d631bd36ab0e10e7e
Valentin Volkl <valentin.volkl@cern.ch>
2018-02-16 20:05:59 +0100

    Format options file

--- Reconstruction/RecTracker/options/TrickTrackSeedingTest.py
+++ Reconstruction/RecTracker/options/TrickTrackSeedingTest.py
@@ -3,46 +3,58 @@ from GaudiKernel import SystemOfUnits as units
 from GaudiKernel import PhysicalConstants as constants
 from Gaudi.Configuration import *
 
-inputfilename2 = "muons_for_seeding.root"
-
-
 from Configurables import FCCDataSvc
-podioevent   = FCCDataSvc("EventDataSvc", input=inputfilename2)
+podioevent   = FCCDataSvc("EventDataSvc", input="muons_for_seeding.root")
+
 from Configurables import PodioInput
-podioinput = PodioInput("PodioReader", collections=["simParticles", "simVertices", "hits", "positionedHits", "trajectory", "trajectoryPoints"], OutputLevel=DEBUG)
+podioinput = PodioInput("PodioReader", 
+                        collections=[
+                                      "simParticles", 
+                                      "simVertices", 
+                                      "hits", 
+                                      "positionedHits", 
+                                      "trajectory", 
+                                      "trajectoryPoints",
+                                      ], 
+                          OutputLevel=DEBUG,
+                          )
 
 from Configurables import GeoSvc
-geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
-  'file:Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml',
-  ],
-  OutputLevel=INFO,
-  )
+geoservice = GeoSvc("GeoSvc", 
+                    detectors=[
+                                'file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
+                                'file:Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml',
+                              ],
+                    OutputLevel=INFO,
+                   )
 
 
-from Configurables import CombinatorialSeedingTest, TrickTrackSeedingTool, BarrelLayerGraphTool
-
-from Configurables import DoubletCreationTool
 
 
+# TrickTrack Seeding Configuration
+from Configurables import BarrelLayerGraphTool
 layergraphtool = BarrelLayerGraphTool()
 
+from Configurables import DoubletCreationTool
 doublet_tool = DoubletCreationTool()
 
-seed_tool = TrickTrackSeedingTool()
-seed_tool.LayerGraphTool = layergraphtool
-seed_tool.seedingLayerIndices0=(0,0)
-seed_tool.seedingLayerIndices1=(0,1)
-seed_tool.seedingLayerIndices2=(0,2)
-seed_tool.seedingLayerIndices3=(0,3)
-seed_tool.readoutName = "TrackerBarrelReadout"
+from Configurables import TrickTrackSeedingTool
+tricktrack_seed_tool = TrickTrackSeedingTool()
+tricktrack_seed_tool.LayerGraphTool = layergraphtool
+tricktrack_seed_tool.seedingLayerIndices0=(0,0)
+tricktrack_seed_tool.seedingLayerIndices1=(0,1)
+tricktrack_seed_tool.seedingLayerIndices2=(0,2)
+tricktrack_seed_tool.seedingLayerIndices3=(0,3)
+tricktrack_seed_tool.readoutName = "TrackerBarrelReadout"
 
+# Alternative: TruthSeeding
 from Configurables import TruthSeedingTool
 truth_seeds = TruthSeedingTool()
 
-combi_seeding = CombinatorialSeedingTest()
-
-combi_seeding.TrackSeedingTool = seed_tool
-combi_seeding.positionedTrackHits.Path = "positionedHits"
+from Configurables import RecTrackAlg
+RecTrackAlg = RecTrackAlg()
+RecTrackAlg.TrackSeedingTool = seed_tool
+RecTrackAlg.positionedTrackHits.Path = "positionedHits"
 
 
 # PODIO algorithm
@@ -53,10 +65,10 @@ out.outputCommands = ["keep *"]
 out.filename="tricktrackSeeding_Example.root"
 
 from Configurables import ApplicationMgr
-ApplicationMgr( TopAlg = [podioinput, combi_seeding, out],
+ApplicationMgr( TopAlg = [podioinput, RecTrackAlg, out],
                 EvtSel = 'NONE',
                 EvtMax   = 3,
                 # order is important, as GeoSvc is needed by SimG4Svc
                 ExtSvc = [podioevent, geoservice],
-                OutputLevel=DEBUG
+                OutputLevel=DEBUG,
  )
