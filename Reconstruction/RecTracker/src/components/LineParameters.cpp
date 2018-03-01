a7313c6e6f6b7fa69defca66a8bb150e08fa5488
William Fawcett <william.fawcett@cern.ch>
2018-03-01 18:27:50 +0100

    add LineParameters class, needs fix with TTPoint

---
+++ Reconstruction/RecTracker/src/components/LineParameters.cpp
@@ -0,0 +1,42 @@
+#include "LineParameters.h"
+
+
+
+void LineParameters::calculateLineParameters(float x0, float y0, float x1, float y1){
+  // Calculate the parameters of the straight line passing through the coordinates (x0, y0), (x1, y1)
+
+  m_gradient = (y0 - y1) / (x0 - x1); 
+  m_y_intercept  = (x1*y0 - x0*y1) / (x1 - x0);
+  m_x_intercept = -1*m_y_intercept/m_gradient; 
+}
+
+
+void LineParameters::simpleLinearLeastSquaresFit() {
+  // Function to do simple linear least squares fitting (for a straight line)
+  // with the parameters y = mx + c. 
+  // Extracts the best fit for m and c. 
+  // Takes a vector of the coordinates {xi, yi} 
+
+  float X(0), Y(0), XX(0), XY(0);
+  float n = m_coordinates.size();
+
+  for(const auto& coord : m_coordinates){
+    float xi = coord.first;
+    float yi = coord.second;
+    X += xi;
+    Y += yi;
+    XX += xi*xi;
+    XY += xi*yi;
+  }
+
+  // gradient
+  m_gradient = (XY*n - X*Y) / (XX*n -X*X);
+
+  // y-intercept
+  m_y_intercept = (Y - m_gradient*X) / n; 
+  
+  // x-intercept
+  m_x_intercept = -1*m_y_intercept/m_gradient;
+
+}
+
---
+++ Reconstruction/RecTracker/src/components/LineParameters.h
@@ -0,0 +1,56 @@
+#ifndef LineParameters_h
+#define LineParameters_h
+
+// convert to new hit class 
+#include "tricktrack/TTPoint.h"
+
+// store parameters of a line 
+class LineParameters {
+  
+  private:
+    std::vector<std::pair<float, float> > m_coordinates;
+    float m_gradient;
+    float m_y_intercept;
+    float m_x_intercept;
+
+  public: 
+
+    // default constructor
+    LineParameters(){
+      m_gradient = 0;
+      m_y_intercept = 0;
+      m_x_intercept =0;
+    }
+
+    // constructor
+    LineParameters(std::vector<std::pair<float, float> > coordinates){
+      m_coordinates = coordinates; 
+    }
+
+    // constructor 
+    /**************
+    LineParameters(std::vector<tricktrack::TTPoint*> hits){
+      for(tricktrack::TTPoint* hit : hits){
+        m_coordinates.push_back( std::make_pair(hit->z(), hit->rho() ) ); // coordinates in (z, r)
+        // m_coordinates.emplace_back( hit->z(), hit->rho() ); // coordinates in (z, r) 
+      }
+    }
+    **************/
+    LineParameters(std::vector<tricktrack::TTPoint> hits){
+      for(const tricktrack::TTPoint hit : hits){
+        m_coordinates.push_back( std::make_pair(hit.z(), hit.rho()) ); // coordinates in (z, r)
+      }
+    }
+
+    // functions to calculate line parameters
+    void simpleLinearLeastSquaresFit();
+    void calculateLineParameters(float x0, float y0, float x1, float y1);
+
+    // accessor functions
+    float gradient() const{ return m_gradient;}
+    float y_intercept() const { return m_y_intercept;}
+    float x_intercept() const { return m_x_intercept;}
+
+};
+
+#endif // LineParameters_h
