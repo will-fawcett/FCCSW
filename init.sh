c95b9a03f8d97a6c374ff0cddab141c45dfce68f
William Fawcett <william.fawcett@cern.ch>
2018-03-16 11:46:52 +0100

    adding missing path

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /cvmfs/fcc.cern.ch/sw/views/nightlies/Fri/x86_64-slc6-gcc62-opt/setup.sh
 
+export FCCPLOTS=/atlas/users/wfawcett/fcc/FCCSW/plots/
+
 
